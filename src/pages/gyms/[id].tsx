d64ae9d56b80490d7b6c6f402609173949daf822
jbilee <julishahn@gmail.com>
2024-01-14 16:44:29 +0900

    feat: 페이지 레이아웃 작업 - 각 컴포넌트가 들어갈 영역을 구분

--- src/pages/gyms/[id].tsx
+++ src/pages/gyms/[id].tsx
@@ -1,9 +1,52 @@
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import styled from 'styled-components';
 
 const GymInfo = ({
   gymData,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  return <div></div>;
+  return (
+    <Styled.Wrapper>
+      <div>헤더 이미지</div>
+      <Styled.InfoContainer>
+        <Styled.Column>
+          <div className="sub-header">
+            암장 주소
+            <br />
+            <div className="sub-name">
+              <span>암장명</span>
+              <span>버튼</span>
+            </div>
+          </div>
+          <div>암장 설명</div>
+          <div>구글지도</div>
+          <div>댓글</div>
+        </Styled.Column>
+        <Styled.Column>
+          <div>관련 태그</div>
+          <div>이용금액</div>
+          <div>영업시간</div>
+          <div>시설 정보</div>
+          <div>난이도</div>
+          <div>연락처</div>
+        </Styled.Column>
+      </Styled.InfoContainer>
+    </Styled.Wrapper>
+  );
+};
+
+const Styled = {
+  Wrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+  InfoContainer: styled.div`
+    display: flex;
+  `,
+  Column: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {

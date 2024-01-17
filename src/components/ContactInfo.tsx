2c691b1a066f583172fd69a60a53811eeff5ddad
jbilee <julishahn@gmail.com>
2024-01-17 13:14:57 +0900

    feat: 연락처 컴포넌트 구현 - 암장 연락처를 연락처 유형에 해당하는 아이콘과 함께 일렬로 표현하는 컴포넌트

---
+++ src/components/ContactInfo.tsx
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+import {
+  BsTwitterX,
+  BsFacebook,
+  BsInstagram,
+  BsTelephoneFill,
+} from 'react-icons/bs';
+
+interface IconsType {
+  [key: string]: JSX.Element;
+}
+
+interface ContactListItem {
+  platform: string;
+  address: string;
+}
+
+// Property 명칭 확정 시 수정
+interface ContactInfoProps {
+  [key: string]: Array<ContactListItem>;
+}
+
+// 상수
+const CONTACT_ICONS: IconsType = {
+  phone: <BsTelephoneFill />,
+  twitter: <BsTwitterX />,
+  facebook: <BsFacebook />,
+  instagram: <BsInstagram />,
+};
+
+const ContactInfo: React.FC<ContactInfoProps> = ({ contactList }) => {
+  // 임시 - 백엔드와 데이터 구조 협의 후 수정 필요
+  const sampleList = [
+    { platform: 'twitter', address: 'sdlfkj' },
+    { platform: 'facebook', address: 'sdlfkj' },
+    { platform: 'instagram', address: 'sdlfkj' },
+    { platform: 'phone', address: '010-1234-5678' },
+  ];
+
+  return (
+    <Styled.Wrapper>
+      {contactList.map((contact) => (
+        <Styled.Item key={contact.platform}>
+          {CONTACT_ICONS[contact.platform]} {contact.address}
+        </Styled.Item>
+      ))}
+    </Styled.Wrapper>
+  );
+};
+
+const Styled = {
+  Wrapper: styled.div`
+    display: flex;
+    gap: 30px;
+    width: 100%;
+  `,
+  Item: styled.div`
+    display: flex;
+    gap: 8px;
+    align-items: center;
+  `,
+};
+
+export default ContactInfo;

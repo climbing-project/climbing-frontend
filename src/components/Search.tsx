a85751d29fd34494f756ef8a3d9d0bbd1ffaf1dd
dmswl6310 <dmswl6310@gmail.com>
2024-01-19 20:31:11 +0900

    feat: input 창 포커스 시 드롭다운 표시, 다른 위치 클릭 시 미표시

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,32 +1,62 @@
+"use client";
+
 import { styled } from "styled-components";
 import { IoSearch } from "react-icons/io5";
 import DropDown, { DropItem } from "./DropDown";
+import { useEffect, useRef, useState } from "react";
 
 interface AddressListProps {
   addressList: Array<DropItem>;
 }
 
 export const Search = ({ addressList }: AddressListProps) => {
+  const searchRef = useRef<HTMLInputElement>(null);
+  const [isInputFocus, setInputFocus] = useState(false);
+
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent): void {
+      if (searchRef.current && !searchRef.current.contains(e.target as Node)) {
+        setInputFocus(false);
+        console.log("hi");
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [searchRef]);
+
+  // const handleInputChange = (event) => {};
   return (
-    <Styled.Wrapper>
+    <Styled.Wrapper ref={searchRef}>
       <Styled.Form>
         <Styled.Input
           type="text"
           placeholder="주소를 입력하면 실내암벽장을 찾아드려요."
+          onClick={(e) => {
+            e.stopPropagation(); // 상위로 이벤트 전송 막음
+          }}
+          onFocus={(e) => {
+            setInputFocus(true);
+          }}
         />
         <IoSearch />
       </Styled.Form>
-      <DropDown
-        prefixIcon={<IoSearch />}
-        dropItems={addressList}
-        width="365px"
-      />
+      {isInputFocus && (
+        <DropDown
+          prefixIcon={<IoSearch />}
+          dropItems={addressList}
+          width="365px"
+        />
+      )}
     </Styled.Wrapper>
   );
 };
 
 const Styled = {
-  Wrapper: styled.div``,
+  Wrapper: styled.div`
+    display: inline-block;
+  `,
   Form: styled.form`
     border: 1px solid black;
     border-radius: 5px;

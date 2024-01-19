e57fd6ac3d273c38067b2f5b94638a2f7dd1767a
jbilee <julishahn@gmail.com>
2024-01-19 18:48:43 +0900

    feat: 암장 등록폼 기초 UI 구현 - 신규 암장 등록 시 사용하는 form 컴포넌트의 레이아웃

---
+++ src/admincomponents/NewGymForm.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+const NewGymForm = () => {
+  return (
+    <Styled.Wrapper>
+      <Styled.Form>
+        <div>
+          <h4>암장명</h4>
+        </div>
+        <div>
+          <h4>암장 주소</h4>
+        </div>
+        <div>
+          <h4>암장 설명</h4>
+        </div>
+        <div>
+          <h4>관련 태그</h4>
+        </div>
+        <div>
+          <h4>이용금액</h4>
+        </div>
+        <div>
+          <h4>영업시간</h4>
+        </div>
+        <div>
+          <h4>시설 정보</h4>
+        </div>
+        <div>
+          <h4>난이도</h4>
+        </div>
+        <div>
+          <h4>연락처</h4>
+        </div>
+        <div>
+          <h4>암장 사진</h4>
+        </div>
+        <input type="submit" name="" />
+      </Styled.Form>
+    </Styled.Wrapper>
+  );
+};
+
+const Styled = {
+  Wrapper: styled.div`
+    h4 {
+      margin-top: 0;
+      margin-bottom: 4px;
+    }
+  `,
+  Form: styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+  `,
+};
+
+export default NewGymForm;

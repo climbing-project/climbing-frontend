543920b3240cf373c37de9c0cc10897029806c9d
dmswl6310 <dmswl6310@gmail.com>
2024-01-29 12:57:35 +0900

    feat: 로그인 provider 추가
    임시로 구글 provider를 활용해 로그인 하는 컴포넌트 생성
    oauth client id와 password 필요

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import Layout from "@/components/Layout";
+import { SessionProvider } from "next-auth/react";
 import { AppContext, AppInitialProps, AppProps } from "next/app";
 import { NextComponentType } from "next/types";
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
-  pageProps,
+  pageProps: { session, ...pageProps },
 }) => {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </Layout>
   );
 };
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,22 @@
 import styled from "styled-components";
+import { useSession, signIn, signOut } from "next-auth/react";
+import LoginButton from "./LoginButton";
 
 const Login = () => {
   return (
-    <form>
-      <input type="id" name="id" placeholder="Id" required />
-      <input type="password" name="password" placeholder="Password" required />
-      <button type="submit">Login</button>
-    </form>
+    <>
+      <form>
+        <input type="id" name="id" placeholder="Id" required />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      <LoginButton />
+    </>
   );
 };
 

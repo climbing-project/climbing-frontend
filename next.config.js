675552df149357be0b1ae879b6490676bed723a2
dmswl6310 <dmswl6310@gmail.com>
2024-01-31 22:53:39 +0900

    test: 테스트를 위한 임시커밋
    문제가 생길시 revert 하겠습니다.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    compiler: {
-      styledComponents: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: false,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+module.exports = nextConfig;

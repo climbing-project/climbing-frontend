75b66e77b5b0a84d487e8703b16732074a02cd9e
dmswl6310 <dmswl6310@gmail.com>
2024-02-12 14:42:31 +0900

    fix: ui와 간편로그인 버튼 수정
    기존 : 세션 사용으로 인해 로그인 버튼 클릭시 리다이렉트페이지를 콜백으로 띄움
    변경 후 : 해당부분 백엔드 에서 처리하기러 결정 (프론트엔드에선 첫화면만 띄움)

--- src/pages/login/GeneralLogin.tsx
+++ src/pages/login/GeneralLogin.tsx
@@ -22,24 +22,39 @@ const GeneralLogin = () => {
   };
 
   return (
-    <S.Wrapper>
-      <form onSubmit={handleSubmit}>
-        <input type="id" name="username" placeholder="username" required />
-        <input
+    <S.Wrapper className="container">
+      <S.LoginForm onSubmit={handleSubmit}>
+        <S.InputBox type="id" name="username" placeholder="아이디" required />
+        <S.InputBox
           type="password"
           name="password"
-          placeholder="Password"
+          placeholder="비밀번호"
           required
         />
-        <button type="submit">Login</button>
-      </form>
+        <S.ButtonBox type="submit">로그인</S.ButtonBox>
+      </S.LoginForm>
     </S.Wrapper>
   );
 };
 
 const S = {
   Wrapper: styled.div`
-    margin-bottom: 100px;
+    height: 160px;
+    padding: 50px;
+    margin-bottom: 30px;
+  `,
+  LoginForm: styled.form`
+    display: flex;
+    flex-direction: column;
+  `,
+  InputBox: styled.input`
+    height: 40px;
+    margin-bottom: 20px;
+  `,
+  ButtonBox: styled.button`
+    height: 40px;
+    background-color: #f9f2f2;
+    border: none;
   `,
 };
 

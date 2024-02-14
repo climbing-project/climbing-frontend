8e433dd7fedfe034e01ae98128fd96059bf33770
jbilee <julishahn@gmail.com>
2024-02-15 01:12:28 +0900

    feat: 관리자페이지/설명글 컴포넌트에 기능 추가 - 설명글을 실제로 수정하고, 수정한 값이 상태에 반영될 수 있도록 구현

--- src/components/admin/DescriptionEditor.tsx
+++ src/components/admin/DescriptionEditor.tsx
@@ -1,14 +1,34 @@
+import { Dispatch, SetStateAction, useState } from 'react';
 import styled from 'styled-components';
+import { GymData } from '@/pages/admin/edit';
 
 interface DescriptionEditorProps {
-  description: string|undefined;
+  description: string;
+  setCurrentData: Dispatch<SetStateAction<GymData | null>>;
 }
 
-const DescriptionEditor = ({ description }: DescriptionEditorProps) => {
+const DescriptionEditor = ({
+  description,
+  setCurrentData,
+}: DescriptionEditorProps) => {
+  const [descriptionInput, setDescriptionInput] = useState(description);
+  const handleChange = (input: string) => {
+    if (input.length > 300) return;
+    setDescriptionInput(input);
+  };
   return (
     <Styled.Wrapper>
       <Styled.Header>설명글</Styled.Header>
-      <Styled.Content>{description}</Styled.Content>
+      <Styled.Content>
+        <Styled.TextField>
+          <textarea
+            value={descriptionInput}
+            onChange={(e) => handleChange(e.target.value)}
+            onBlur={(e) => setCurrentData((prev) => ({...prev, description: e.target.value} as GymData))}
+          />
+        </Styled.TextField>
+        {descriptionInput.length}/300
+      </Styled.Content>
     </Styled.Wrapper>
   );
 };
@@ -30,6 +50,27 @@ const Styled = {
     flex-direction: ${(props) => props.$direction};
     flex-wrap: wrap;
     gap: 20px;
+  `,
+  TextField: styled.div`
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    gap: 6px;
+    background: #fafafa;
+    border-radius: 8px;
+    border: 1px solid #d0d0d0;
+    padding: 12px 18px;
+    width: 100%;
+    height: 150px;
+
+    textarea {
+      border: none;
+      background: transparent;
+      width: 100%;
+      height: 100%;
+      padding: 0;
+      resize: none;
+    }
   `,
 };
 

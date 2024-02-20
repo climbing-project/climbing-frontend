3b9717e401af3ccd80360cf0d5d948c77f4ac51d
dmswl6310 <dmswl6310@gmail.com>
2024-02-20 23:13:51 +0900

    feat: 회원가입 field별 유효성 검사 추가
    email => 이메일 형식 필요(emailRegrex)
    password=>영어+숫자+특수문자 8 자이상
    nickname => 3자이상(임시)

--- src/components/InputWithTitle.tsx
+++ src/components/InputWithTitle.tsx
@@ -1,50 +1,30 @@
-import { Dispatch, SetStateAction } from "react";
 import { styled } from "styled-components";
 
 interface InputProps {
   name: string;
   title: string;
   type?: string;
-  setValidity?: Dispatch<SetStateAction<boolean>>;
-  validity?: boolean;
+  onChange?: (event: { target: { value: string } }) => Promise<void> | void;
+  message?: string;
 }
 
-export const InputWithTitle = ({
+const InputWithTitle = ({
   name,
   title,
   type = "text",
-  setValidity,
-  validity,
+  onChange,
+  message = "",
 }: InputProps) => {
-  const handleChange = async (event: {
-    target: {
-      value: string;
-      name: string;
-    };
-  }) => {
-    if (!setValidity) return;
-    // const res = await fetch(`http://localhost:3000/members/${name}Check`);
-    // const data = await res.json();
-
-    // 서버 없어서 임시 로직 적용(5자 이상일시 valid)
-    if (event.target.value.length >= 5) {
-      // 중복이 없을때 사용가능
-      setValidity(true);
-    } else {
-      setValidity(false);
-    }
-  };
-
-  const handleWarningText = () => {
-    return setValidity && !validity ? `중복된 ${name}이 존재합니다` : "";
-    // return <Styled.Warning>text</Styled.Warning>;
-  };
-
   return (
     <Styled.Wrapper>
       <Styled.Title>{title}</Styled.Title>
-      <Styled.Input name={name} type={type} onChange={handleChange} />
-      <Styled.Warning>{handleWarningText()}</Styled.Warning>
+      <Styled.Input
+        $hasMessage={message !== ""}
+        name={name}
+        type={type}
+        onChange={onChange}
+      />
+      <Styled.Warning>{message}</Styled.Warning>
     </Styled.Wrapper>
   );
 };
@@ -56,8 +36,9 @@ const Styled = {
     margin-bottom: 15px;
   `,
   Title: styled.div``,
-  Input: styled.input`
+  Input: styled.input<{ $hasMessage: boolean }>`
     height: 30px;
+    outline-color: ${(props) => (props.$hasMessage ? "red" : "green")};
   `,
   Warning: styled.div`
     height: 10px;

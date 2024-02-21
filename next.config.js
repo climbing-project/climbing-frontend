249812b3832fd77de90787a6596a52d174d9a044
dmswl6310 <dmswl6310@gmail.com>
2024-02-21 17:11:47 +0900

    feat: nextjs에서 도커 배포를 위한 도커파일 설정
    3000번으로 포트
    standalone으로 output

--- next.config.js
+++ next.config.js
@@ -3,16 +3,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "standalone",
   compiler: {
     styledComponents: true,
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'oruritest.s3.ap-northeast-2.amazonaws.com',
-        port: '',
-        pathname: '/bubu/*',
+        protocol: "https",
+        hostname: "oruritest.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/bubu/*",
       },
     ],
   },

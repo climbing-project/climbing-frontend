c5a22117d73a65aa84dfac7f94d87625971d81b8
dmswl6310 <dmswl6310@gmail.com>
2024-03-06 21:30:32 +0900

    feat: 마이페이지 항목 추가
    로그인 되어있지 않을시 에러페이지 표시

--- src/components/settings/Mypage.tsx
+++ src/components/settings/Mypage.tsx
@@ -1,5 +1,41 @@
+import { useSession } from "next-auth/react";
+
 const Mypage = () => {
-  return <>내 정보</>;
+  const { data: session, status } = useSession();
+
+  if (status !== "authenticated") {
+    return <div>잘못된 접근입니다.</div>;
+  }
+
+  return (
+    <table>
+      <thead></thead>
+      <tbody>
+        <tr>
+          <td>아이디(이메일)</td>
+          <td>session.user!.email!</td>
+        </tr>
+        <tr>
+          <td>비밀번호</td>
+          <td>
+            <input></input>
+          </td>
+        </tr>
+        <tr>
+          <td>비밀번호 재확인</td>
+          <td>
+            <input></input>
+          </td>
+        </tr>
+        <tr>
+          <td>닉네임</td>
+          <td>
+            <input></input>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default Mypage;

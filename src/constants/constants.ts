c2e113522ff26820e6f61d77cffd35d9e3c57443
jbilee <julishahn@gmail.com>
2024-04-02 00:17:19 +0900

    refactor: 에러 핸들링을 위한 코드 수정 - 백엔드 테스트 시 발생할 에러를 콘솔에서 확인하기 위해 암장 상세 페이지에 fetch 관련 코드 일부 수정

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,11 +1,7 @@
 // 네이버 지도 API
-export const NAVERMAP_API =
-  'https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=lm660e08li';
-
+export const NAVERMAP_API = "https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=lm660e08li";
 export const NAVERMAP_GEOCODE_API =
-  'https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=lm660e08li&submodules=geocoder';
+  "https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=lm660e08li&submodules=geocoder";
 
-// 서버 API
-export const GYM_API = 'http://localhost:3000/gyms/';
-
-export const MEMBER_API = 'http://localhost:3000/members/';
+// 서버 주소
+export const SERVER_ADDRESS = "http://192.168.135.143:8080";

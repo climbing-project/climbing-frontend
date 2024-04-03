2c27ecf369dfd0b0791b2668c1d1c2962d3f7bc3
jbilee <julishahn@gmail.com>
2024-04-04 00:21:28 +0900

    refactor: 관리자페이지 메인 화면 리팩토링 - 조건부 렌더링을 사용해 등록된 암장이 있는지 여부에 따라 다른 화면을 보여주도록 구조 수정

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
+import styled from "styled-components";
 import AdminLayout from "@/components/admin/AdminLayout";
 import Overview from "@/components/admin/Overview";
 
@@ -7,9 +9,16 @@ const AdminHome = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
 
+  // 유저에게 암장 정보가 매핑되어 있는 경우에만 Overview 렌더링
+
   return (
     <AdminLayout>
       <Overview />
+      <HR />
+      <Text>현재 관리하고 있는 암장이 없습니다. 내 암장을 사이트에 등록해 보아요!</Text>
+      <Link href={"/admin/register"}>
+        <Btn>암장 등록하기</Btn>
+      </Link>
     </AdminLayout>
   );
 
@@ -20,9 +29,30 @@ const AdminHome = () => {
 
   // return !session ? null : (
   //   <>
-  //     <AdminLayout>default page</AdminLayout>
+  //     <AdminLayout><Overview /></AdminLayout>
   //   </>
   // );
 };
+
+const HR = styled.hr`
+  border: 2px dashed gray;
+  width: 100%;
+`;
+
+const Text = styled.div`
+  text-align: center;
+`;
+
+const Btn = styled.div`
+  background: #307fe5;
+  color: white;
+  padding: 24px;
+  border-radius: 12px;
+  display: grid;
+  width: 160px;
+  place-content: center center;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 export default AdminHome;

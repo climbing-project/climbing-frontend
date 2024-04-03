1e505a96aa061d26edfd089c2f842228ae4cfae4
jbilee <julishahn@gmail.com>
2024-04-03 12:07:34 +0900

    feat: ErrorBoundary 컴포넌트 구현 - 에러 발생 시 fallback할 수 있는 컴포넌트 생성

--- src/pages/admin/edit/index.tsx
+++ src/pages/admin/edit/index.tsx
@@ -1,15 +1,16 @@
 import { ReactElement } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { useRouter } from "next/router";
 import AdminLayout from "@/components/admin/AdminLayout";
-import EditPage1 from "../../../components/admin/EditPage1";
-import EditPage2 from "../../../components/admin/EditPage2";
+import EditPage1 from "@/components/admin/EditPage1";
+import EditPage2 from "@/components/admin/EditPage2";
+import { ErrorFallback } from "@/components/common/ErrorFallback";
 import Layout from "@/components/Layout";
-import { NextPageWithLayout } from "@/pages/_app";
+import type { NextPageWithLayout } from "@/pages/_app";
 
 const EditPage: NextPageWithLayout = () => {
   const router = useRouter();
   const { page } = router.query;
-
   if (page === "2") return <EditPage2 />;
   else return <EditPage1 />;
 };
@@ -17,7 +18,9 @@ const EditPage: NextPageWithLayout = () => {
 EditPage.getLayout = (page: ReactElement) => {
   return (
     <Layout>
-      <AdminLayout>{page}</AdminLayout>
+      <AdminLayout>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>{page}</ErrorBoundary>
+      </AdminLayout>
     </Layout>
   );
 };

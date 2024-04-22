dbcbb1c7791e300c238fcc6f1ac214411af34246
dmswl6310 <dmswl6310@gmail.com>
2024-04-22 14:22:45 +0900

    refactor: 코드리팩토링

--- src/components/login/OtherLogin.tsx
+++ src/components/login/OtherLogin.tsx
@@ -1,67 +1,42 @@
 import styled from "styled-components";
 import Image from "next/image";
-import { useSession, signIn, signOut } from "next-auth/react";
 import NaverIcon from "../../../public/naver_rec.png";
 import GoogleIcon from "../../../public/google_rec.png";
 import KakaoIcon from "../../../public/kakao_rec.png";
 import Link from "next/link";
-import { SERVER_ADDRESS } from "@/constants/constants";
 
-// 세션 사용시의 코드
-// const OtherLogin = () => {
-//   const { data: session } = useSession();
-
-//   if (session) {
-//     return (
-//       <>
-//         Signed in as {session?.user?.email} <br />
-//         <button onClick={() => signOut()}>Sign out</button>
-//       </>
-//     );
-//   }
-//   return (
-//     <>
-//       Not signed in <br />
-//       <button onClick={() => signIn()}>다른 방법으로 로그인</button>
-//     </>
-//   );
-// };
-const handleKakaoLogin = () => {
-  // signIn("kakao", {
-  //   // callbackUrl: "/login/oauth2/code/kakao",
-  // });
-};
-
-// 백엔드에서 세션 처리시 사용할 코드
+// 백엔드로 리다이렉트(백엔드에서 인가코드->토큰받고 프론트로 보내줌)
 const OtherLogin = () => {
   return (
     <S.Wrapper>
       <div>간편로그인</div>
       <S.IconContainer>
         <Link
-          href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NAVER_CLIENT_ID}&redirect_uri=${process.env.NAVER_REDIRECT_URI}&state=${process.env.NAVER_STATE}`}
+          href={`https://nid.naver.com/oauth2.0/authorize?
+          response_type=code
+          &client_id=${process.env.NAVER_CLIENT_ID}
+          &redirect_uri=${process.env.NAVER_REDIRECT_URI}
+          &state=${process.env.NAVER_STATE}`}
         >
           <Image src={NaverIcon} alt="네이버 아이콘" height={30} />
         </Link>
         <Link
           href={`https://accounts.google.com/o/oauth2/v2/auth?
-		client_id=${process.env.GOOGLE_CLIENT_ID}
-		&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}
-		&response_type=code
-		&scope=email profile`}
+          client_id=${process.env.GOOGLE_CLIENT_ID}
+		      &redirect_uri=${process.env.GOOGLE_REDIRECT_URI}
+		      &response_type=code
+		      &scope=email profile`}
         >
           <Image src={GoogleIcon} alt="구글 아이콘" height={30} />
         </Link>
         <Link
-          href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}\
-&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code&scope=account_email`}
+          href={`https://kauth.kakao.com/oauth/authorize?
+          client_id=${process.env.KAKAO_CLIENT_ID}
+          &redirect_uri=${process.env.KAKAO_REDIRECT_URI}
+          &response_type=code
+          &scope=account_email`}
         >
-          <Image
-            src={KakaoIcon}
-            alt="카카오 아이콘"
-            height={30}
-            // onClick={handleKakaoLogin}
-          />
+          <Image src={KakaoIcon} alt="카카오 아이콘" height={30} />
         </Link>
       </S.IconContainer>
     </S.Wrapper>

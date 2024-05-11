6fd1064c8f7215ed1529e0c2b9d7f2418e324b1a
jbilee <julishahn@gmail.com>
2024-05-11 11:47:14 +0900

    fix: 암장 상세 페이지 버그 수정 - 자체 상태값을 갖고 있는 컴포넌트가 부모 컴포넌트로 인해 리렌더링되었을 때 상태값이 초기화되지 않던 이슈 수정

--- src/pages/gyms/[id].tsx
+++ src/pages/gyms/[id].tsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import Error from "next/error";
 import styled from "styled-components";
 import Comments from "@/components/gyms/Comments";
 import DynamicMap from "@/components/gyms/DynamicMap";
+import ErrorPage from "@/components/common/ErrorPage";
 import HelpModal from "@/components/chat/HelpModal";
 import ImageCarousel from "@/components/gyms/ImageCarousel";
 import MainContent from "@/components/gyms/MainContent";
 import SideContent from "@/components/gyms/SideContent";
 import useApi from "@/hooks/useApi";
+import { requestData } from "@/service/api";
 import { DEVICE_SIZE } from "@/constants/styles";
 import { IMAGE_SIZE } from "@/constants/gyms/constants";
-import { NAVERMAP_API, SERVER_ADDRESS } from "@/constants/constants";
+import { NAVERMAP_API, SERVER_ADDRESS, TEST_ADDRESS } from "@/constants/constants";
 import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 const GymInfo = ({
@@ -27,22 +28,25 @@ const GymInfo = ({
 
   const toggleModal = () => setIsOpen((prev) => !prev);
 
-  const handlePageLeave = () => {
-    if (isOpen) setIsOpen(false);
-  };
-
   useEffect(() => {
+    const handlePageLeave = () => {
+      if (isOpen) setIsOpen(false);
+    };
     router.events.on("routeChangeStart", handlePageLeave);
+
+    return () => router.events.off("routeChangeStart", handlePageLeave);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (error) return <Error statusCode={statusCode} />;
+  if (error) return <ErrorPage statusCode={statusCode} />;
   return (
     <S.Page>
       <S.Wrapper>
-        {!gymData.defaultImage && !gymData.images ? null : (
-          <ImageCarousel defaultImage={gymData.defaultImage} imageList={gymData.images} />
-        )}
+        <ImageCarousel
+          key={crypto.randomUUID()}
+          defaultImage={gymData.defaultImage}
+          imageList={gymData.images}
+        />
         <S.InfoContainer>
           <S.Main>
             <MainContent gymData={gymData} />
@@ -50,7 +54,12 @@ const GymInfo = ({
           </S.Main>
           <SideContent gymData={gymData} />
         </S.InfoContainer>
-        <Comments id={gymData.id} comments={gymData.comments} session={session} />
+        <Comments
+          key={crypto.randomUUID()}
+          id={gymData.id}
+          comments={gymData.comments}
+          session={session}
+        />
       </S.Wrapper>
       <HelpModal
         gymId={gymData.id}
@@ -144,24 +153,15 @@ const S = {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const gymId = context.query.id;
+  const controller = new AbortController();
+  setTimeout(() => controller.abort(), 3000);
   try {
-    const response = await Promise.race([
-      fetch(`${SERVER_ADDRESS}/gyms/${gymId}`),
-      new Promise<Response>((_, reject) =>
-        setTimeout(() => reject(new Response(null, { status: 503 })), 3000),
-      ),
-    ]);
+    const response = await fetch(`${TEST_ADDRESS}/gyms/${gymId}`, { signal: controller.signal });
     if (response.status === 200) {
       const gymData = await response.json();
       return { props: { gymData } };
     } else throw response.status;
   } catch (statusCode) {
-    // 404에러 시에도 데이터를 채우기 위한 임시방편
-    console.log("*****Server fetch failed. Fetching from local json-server instead*****");
-    const gymData = await (await fetch(`http://localhost:8000/gyms/${gymId}`)).json();
-    return { props: { gymData } };
-
-    // 테스트 완료 시 아래 코드로 교체
     return { props: { error: true, statusCode } };
   }
 };

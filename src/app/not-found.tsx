95c6d2b9e0b469478c181b26edfc1cd664b2c30e
dmswl6310 <dmswl6310@gmail.com>
2024-05-16 20:21:53 +0900

    feat: 404 없는페이지 접근시 표시되는 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import router from "next/router";
+import { styled } from "styled-components";
+
+export default function NotFound() {
+  const handleHomeBtn = () => {
+    router.push("/home");
+  };
+  const handleLoginBtn = () => {
+    router.push("/login");
+  };
+
+  return (
+    <S.Container>
+      <h1>잘못된 페이지 입니다.</h1>
+      <h4>에러코드 : 404</h4>
+      <S.ButtonContainer>
+        <S.HomeButton onClick={handleHomeBtn}>홈으로</S.HomeButton>
+        <S.LoginButton onClick={handleLoginBtn}>로그인하기</S.LoginButton>
+      </S.ButtonContainer>
+      <Link href="/">홈으로 돌아가기</Link>
+    </S.Container>
+  );
+}
+
+const S = {
+  Container: styled.div`
+    height: 700px;
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 auto;
+    text-align: center;
+  `,
+  ButtonContainer: styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 30px;
+  `,
+  HomeButton: styled.button`
+    flex-grow: 1;
+    margin: 0 10px;
+  `,
+  LoginButton: styled.button`
+    flex-grow: 3;
+    margin: 0 10px;
+  `,
+};

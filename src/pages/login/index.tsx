aea86de776bcf9d4bb0e3d8e74ef6831b66f380e
dmswl6310 <dmswl6310@gmail.com>
2024-05-25 21:21:13 +0900

    test: auth url 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -20,7 +20,7 @@ const Login = () => {
           refreshToken: refreshToken,
           type: "oauth",
           redirect: true,
-          callbackUrl: "http://13.125.164.197:443/",
+          callbackUrl: "/",
         });
 
         if (result?.error) {

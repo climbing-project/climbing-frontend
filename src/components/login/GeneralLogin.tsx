ec3075b89b48df0adb78a3ff5d31481adfdbfd97
dmswl6310 <dmswl6310@gmail.com>
2024-05-27 22:49:00 +0900

    fix: 롤백

--- src/components/login/GeneralLogin.tsx
+++ src/components/login/GeneralLogin.tsx
@@ -17,7 +17,7 @@ const GeneralLogin = () => {
       password: password,
       type: "normal",
       redirect: true,
-      callbackUrl: "/",
+      callbackUrl: "/home",
     });
 
     if (result?.error) {

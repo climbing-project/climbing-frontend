c7117e067ed522eb8203b3cf967371361dc81943
dmswl6310 <dmswl6310@gmail.com>
2024-05-27 21:26:04 +0900

    fix: redirect 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -18,7 +18,7 @@ const Login = () => {
           accessToken: accessToken,
           refreshToken: refreshToken,
           type: "oauth",
-          redirect: false,
+          redirect: true,
           callbackUrl: "/",
         });
 

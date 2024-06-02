e65a3a74df7ee18ea1915e51f5ba5e72a5f2668f
dmswl6310 <dmswl6310@gmail.com>
2024-06-02 14:02:06 +0900

    test

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -49,6 +49,55 @@ const Test = () => {
     return session?.jwt;
   };
 
+  const testFunc2 = async () => {
+    alert("테스트 키2 입력됨");
+    const data = {
+      email: "eunjitest90@gmail.com",
+      password: "hello1234!",
+    };
+    const result = await fetch(`${SERVER_ADDRESS}/members/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`${res.status} 에러`);
+        }
+        return res;
+      })
+      .then(async (response) => {
+        const responseHeaders = response.headers;
+        const responseAccessToken = responseHeaders.get("Authorization");
+        const responseRefreshToken = responseHeaders.get(
+          "Authorization-refresh"
+        );
+        if (!(responseHeaders && responseAccessToken && responseRefreshToken)) {
+          throw Error("missing header or token");
+        }
+
+        // 받은 토큰
+        const jwt = {
+          accessToken: responseAccessToken || "tempAccess",
+          refreshToken: responseRefreshToken || "tempRefresh",
+        };
+
+        const body = await response.json();
+        // const email = body.email || "tempEmail";
+        // const nickname = body.nickname || "tempNickname";
+        const email = body.email || "tempEmail";
+        const nickname = body.nickname || "tempNickname";
+        return { user: { email: email, nickname: nickname }, jwt };
+      })
+      .catch((error) => {
+        console.log("\n로그인에러");
+        console.log("옵션 : POST");
+        console.log(error.stack + "\n");
+      });
+    console.log(result);
+    return result as any;
+  };
+
   const keyDown = async (event: {
     key: string;
     preventDefault: () => void;
@@ -56,6 +105,10 @@ const Test = () => {
     if (event.key === "t") {
       event.preventDefault();
       const data = await testFunc();
+      console.log(data);
+    } else if (event.key === "d") {
+      event.preventDefault();
+      const data = await testFunc2();
       console.log(data);
     }
   };

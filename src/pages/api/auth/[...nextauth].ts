e346d8b1b0d3301536fe08bf0fd8d20b2119876f
dmswl6310 <dmswl6310@gmail.com>
2024-06-02 15:04:01 +0900

    fix

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,9 +17,9 @@ export default NextAuth({
       },
       async authorize(credentials: any) {
         console.log("일반 로그인");
-        if (!credentials.email || !credentials.password) {
-          return null;
-        }
+        // if (!credentials.email || !credentials.password) {
+        //   return null;
+        // }
         const tempResult = {
           user: { email: "tempEmail", nickname: "tempNickname" },
           jwt: { accessToken: "tempAccess", refreshToken: "tempRefresh" },

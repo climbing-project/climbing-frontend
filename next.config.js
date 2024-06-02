17702edde752737aa717ab37beb7868c50e0747b
dmswl6310 <dmswl6310@gmail.com>
2024-06-02 17:35:28 +0900

    change config

--- next.config.js
+++ next.config.js
@@ -37,7 +37,6 @@ const nextConfig = {
         protocol: "https",
         hostname: "oruritest.s3.ap-northeast-2.amazonaws.com",
         port: "",
-        pathname: "/bubu/*",
       },
     ],
   },

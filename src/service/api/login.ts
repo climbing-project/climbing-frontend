8dd5abb539cfeaf18883be56d6e8ba50cd46a61e
dmswl6310 <dmswl6310@gmail.com>
2024-06-02 18:53:56 +0900

    fix content

--- src/service/api/login.ts
+++ src/service/api/login.ts
@@ -5,7 +5,7 @@ const getLoginInfos = async (email: string, password: string) => {
 
   return await fetch(`${SERVER_ADDRESS}/members/login`, {
     method: "POST",
-    headers: { "Content-Type": "application/json", "Accept-Language": "en-US" },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(sendInfo),
   })
     .then((res) => {

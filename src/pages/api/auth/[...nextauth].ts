b23a94a4b4296ce9d2c2923d39d7a020d8d40c73
dmswl6310 <dmswl6310@gmail.com>
2024-06-02 22:57:31 +0900

    fix redirect

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -131,27 +131,27 @@ export default NextAuth({
 
   //  jwt나 세션 쓸때
   callbacks: {
-    // async redirect({ url, baseUrl }) {
-    //   // let result = url;
-    //   // if (url.startsWith("/")) {
-    //   //   result = `${baseUrl}${url}`;
-    //   // }
-    //   // return result;
-    //   // Allows relative callback URLs
-    //   console.log(`url: ${url}`);
-    //   console.log(`baseUrl: ${baseUrl}`);
-    //   if (url.startsWith("/")) {
-    //     console.log("1");
-    //     return `${baseUrl}${url}`;
-    //   }
-    //   // Allows callback URLs on the same origin
-    //   else if (new URL(url).origin === baseUrl) {
-    //     console.log("2");
-    //     return url;
-    //   }
-    //   console.log("3");
-    //   return baseUrl;
-    // },
+    async redirect({ url, baseUrl }) {
+      // let result = url;
+      // if (url.startsWith("/")) {
+      //   result = `${baseUrl}${url}`;
+      // }
+      // return result;
+      // Allows relative callback URLs
+      console.log(`url: ${url}`);
+      console.log(`baseUrl: ${baseUrl}`);
+      if (url.startsWith("/")) {
+        console.log("1");
+        return `${baseUrl}${url}`;
+      }
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) {
+        console.log("2");
+        return url;
+      }
+      console.log("3");
+      return baseUrl;
+    },
 
     // 로그인 시 return한 값이 user로 들어옴
     async jwt({ token, user }) {

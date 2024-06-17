408a2dd57339abb52de2868af824c2e9f23f4483
dmswl6310 <dmswl6310@gmail.com>
2024-06-17 11:58:23 +0900

    style: 설정 사이드바 ui 변경

--- src/components/settings/SettingLayout.tsx
+++ src/components/settings/SettingLayout.tsx
@@ -1,39 +1,51 @@
-import Link from "next/link";
 import { styled } from "styled-components";
+import { COLOR } from "@/styles/global-color";
+import { useRouter } from "next/router";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const SettingLayout = ({ children }: React.PropsWithChildren<{}>) => {
+  const router = useRouter();
+  const { page } = router.query;
+
+  function handleMenuClick(page: string) {
+    router.push({ pathname: "/settings", query: { page: page } });
+  }
+
   return (
     <S.Wrapper>
       <S.Sidebar>
-        <h3>설정</h3>
-        <Link
+        <S.TitleContainer>
+          <IoSettingsOutline size={27} />
+          <S.Title>설정</S.Title>
+        </S.TitleContainer>
+        <S.Menu
+          $isClicked={page === "myBookmark"}
           className="link-plain"
-          href={{ pathname: "/settings", query: { page: "myBookmark" } }}
-          as="/settings/myBookmark"
+          onClick={() => handleMenuClick("myBookmark")}
         >
           내 북마크 관리
-        </Link>
-        <Link
+        </S.Menu>
+        <S.Menu
+          $isClicked={page === "myPage"}
           className="link-plain"
-          href={{ pathname: "/settings", query: { page: "myPage" } }}
-          as="/settings/myPage"
+          onClick={() => handleMenuClick("myPage")}
         >
           내 정보 수정
-        </Link>
-        <Link
+        </S.Menu>
+        <S.Menu
+          $isClicked={page === "changePassword"}
           className="link-plain"
-          href={{ pathname: "/settings", query: { page: "changePassword" } }}
-          as="/settings/changePassword"
+          onClick={() => handleMenuClick("changePassword")}
         >
           비밀번호 변경
-        </Link>
-        <Link
+        </S.Menu>
+        <S.Menu
+          $isClicked={page === "deleteAccount"}
           className="link-plain"
-          href={{ pathname: "/settings", query: { page: "deleteAccount" } }}
-          as="/settings/deleteAccount"
+          onClick={() => handleMenuClick("deleteAccount")}
         >
           계정 탈퇴
-        </Link>
+        </S.Menu>
       </S.Sidebar>
       <S.Main>{children}</S.Main>
     </S.Wrapper>
@@ -45,26 +57,42 @@ const S = {
     display: flex;
     justify-content: space-between;
   `,
+  TitleContainer: styled.div`
+    display: flex;
+    align-items: center;
+  `,
+  Title: styled.h2`
+    margin-left: 10px;
+  `,
   Sidebar: styled.div`
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    padding: 36px 42px;
-    width: 320px;
+    padding: 0 15px;
+    /* gap: 10px; */
     flex-shrink: 0;
-    gap: 12px;
-    border-right: 1px solid #d0d0d0;
+    width: 250px;
+    border-right: 1px solid ${COLOR.BORDER_UNFOCUSED};
+  `,
+  Menu: styled.button<{ $isClicked: boolean }>`
+    width: 100%;
+    height: 50px;
+    border: 1px solid white;
+    border-color: ${({ $isClicked }) => ($isClicked ? COLOR.MAIN : "white")};
+    color: ${({ $isClicked }) => ($isClicked ? COLOR.MAIN : "black")};
+    border-radius: 5px;
+    background: white;
+    font-weight: ${({ $isClicked }) => ($isClicked ? "bold" : "400")};
   `,
   Main: styled.div`
     display: flex;
     flex-direction: column;
     flex: 1 0 0;
-    gap: 36px;
+    gap: 10px;
     background: #fafaf8;
     padding: 36px;
   `,
-  Link: styled.a``,
 };
 
 export default SettingLayout;
--- src/styles/global-color.ts
+++ src/styles/global-color.ts
@@ -3,5 +3,5 @@ export const enum COLOR {
   LIGHT_MAIN = "#b1d3ff",
   DISABLED = "#cacaca",
   WARNING = "#FFB1B1",
-  BORDER_UNFOCUSED = "#ced4da",
+  BORDER_UNFOCUSED = "#d0d0d0",
 }
